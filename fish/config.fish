bebf5f8d3b0ad46720255e774053757b2bcc6843
watal <watal.i27e@gmail.com>
2018-01-23 11:23:08 +0900

    fix fish path

--- fish/config.fish
+++ fish/config.fish
@@ -10,8 +10,7 @@ set -g theme_display_user yes
 set -x LANG en_US
 set theme_color_scheme zenburn
 
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set PATH $PATH /usr/local/sbin
+set -U fish_user_paths "/usr/local/sbin"
 
 # tex
 set -x PATH /Library/TeX/texbin $PATH
