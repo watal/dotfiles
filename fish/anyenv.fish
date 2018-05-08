2e4f576f38c1a7f52bafaed2616660b47c5653b3
watal <watal.i27e@gmail.com>
2018-05-08 19:10:59 +0900

    PATH

--- fish/anyenv.fish
+++ fish/anyenv.fish
@@ -1,12 +1,18 @@
 # anyenv
-set -x PATH $usr/local/Cellar/anyenv/3cb8ad1/bin $PATH
+source "/usr/local/Cellar/anyenv/3cb8ad1/libexec/../completions/anyenv.fish"
+function anyenv
+  set command $argv[1]
+  set -e argv[1]
+
+  command anyenv "$command" $argv
+end
 
 # pyenv
 set -x PYENV_ROOT "/usr/local/Cellar/anyenv/3cb8ad1/envs/pyenv"
-set -x PATH $PATH "/usr/local/Cellar/anyenv/3cb8ad1/envs/pyenv/bin"
-set -gx PATH '/usr/local/Cellar/anyenv/3cb8ad1/envs/pyenv/shims' $PATH
-set -gx PYENV_SHELL fish
-source '/usr/local/Cellar/anyenv/3cb8ad1/envs/pyenv/libexec/../completions/pyenv.fish'
+set -x PATH "/usr/local/Cellar/anyenv/3cb8ad1/envs/pyenv/bin" $PATH
+set -x PATH "/usr/local/Cellar/anyenv/3cb8ad1/envs/pyenv/shims" $PATH
+set -x PYENV_SHELL fish
+source "/usr/local/Cellar/anyenv/3cb8ad1/envs/pyenv/libexec/../completions/pyenv.fish"
 command pyenv rehash 2>/dev/null
 function pyenv
   set command $argv[1]
@@ -20,12 +26,12 @@ function pyenv
   end
 end
 
-# rbenv
+#rbenv
 set -x RBENV_ROOT "/usr/local/Cellar/anyenv/3cb8ad1/envs/rbenv"
-set -x PATH $PATH "/usr/local/Cellar/anyenv/3cb8ad1/envs/rbenv/bin"
-set -gx PATH '/usr/local/Cellar/anyenv/3cb8ad1/envs/rbenv/shims' $PATH
-set -gx RBENV_SHELL fish
-source '/usr/local/Cellar/anyenv/3cb8ad1/envs/rbenv/libexec/../completions/rbenv.fish'
+set -x PATH "/usr/local/Cellar/anyenv/3cb8ad1/envs/rbenv/bin" $PATH
+set -x PATH "/usr/local/Cellar/anyenv/3cb8ad1/envs/rbenv/shims" $PATH
+set -x RBENV_SHELL fish
+source "/usr/local/Cellar/anyenv/3cb8ad1/envs/rbenv/libexec/../completions/rbenv.fish"
 command rbenv rehash 2>/dev/null
 function rbenv
   set command $argv[1]
