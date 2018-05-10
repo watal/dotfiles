441198df5f9a325c55a0f7099e65b2112fe0f924
watal <watal.i27e@gmail.com>
2018-05-10 18:15:18 +0900

    titlebar

--- fish/config.fish
+++ fish/config.fish
@@ -17,6 +17,4 @@ set -x PATH /usr/local/opt/curl/bin $PATH
 set -x PATH /usr/local/opt/openssl/bin $PATH
 
 # iterm
-echo -ne "\033]6;1;bg;red;brightness;\a"
-echo -ne "\033]6;1;bg;green;brightness;\a"
-echo -ne "\033]6;1;bg;blue;brightness;\a"
+echo -ne "\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;0\a\033]6;1;bg;blue;brightness;0\a"
