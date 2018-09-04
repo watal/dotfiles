ab5aada56105693cfdcb01b28cc6bf1d6f81c026
watal <watal.i27e@gmail.com>
2018-09-04 10:31:25 +0900

    Move file

--- fish/fzf.fish
+++
@@ -1,4 +0,0 @@
-set -x FZF_TMUX_HEIGHT "40%"
-set -x FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT"
-set -x FZF_LEGACY_KEYBINDINGS 1
-set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
