310df1d45b5ee4971588e91882fea9ade0f942d8
watal <watal.i27e@gmail.com>
2025-03-18 02:01:59 +0700

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,32 @@
-#!/bin/sh -
+#!/usr/bin/env bash
 
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
+set -euo pipefail
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
-if [ `uname` == 'Darwin' ]; then
-    echo $'\e[32mStart setting up dotfiles on macOS.\e[0m'
-    $SCRIPT_DIR/setup/macOS/setup_darwin.sh $SCRIPT_DIR
-    echo $'\e[32mFinish setting up dotfiles.\e[0m'
+if [ "$(uname)" = "Darwin" ]; then
+    echo -e "\e[32mStart setting up dotfiles on macOS.\e[0m"
+
+    # Install Xcode Command Line Tools
+    if ! xcode-select -p &>/dev/null; then
+        echo -e "\e[33mInstalling Xcode Command Line Tools...\e[0m"
+        xcode-select --install
+        until xcode-select -p &>/dev/null; do
+            sleep 5
+        done
+    fi
+
+    # Install Homebrew
+    if ! command -v brew &>/dev/null; then
+        echo -e "\e[33mInstalling Homebrew...\e[0m"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        eval "$(/opt/homebrew/bin/brew shellenv)"  # Set up Homebrew for Apple Silicon
+    fi
+
+    # Run macOS setup script
+    "$SCRIPT_DIR/setup/macOS/setup_darwin.sh" "$SCRIPT_DIR"
+
+    echo -e "\e[32mFinish setting up dotfiles.\e[0m"
 else
-    echo $'\e[32mSorry, this OS is not yet supported.\e[0m'
+    echo -e "\e[31mSorry, this OS is not yet supported.\e[0m"
+    exit 1
 fi
--- setup/macOS/setup_darwin.sh
+++ setup/macOS/setup_darwin.sh
@@ -1,60 +1,42 @@
-#!/bin/sh -
+#!/usr/bin/env bash
 
-# 第1引数がない場合は2階層上のファイルパスを取得
-CWD=${1:-$(cd $(dirname $0)/../../; pwd)}
+set -euo pipefail
+CWD=${1:-$(cd "$(dirname "$0")"/../../ && pwd)}
 
-
-# pip3のパッケージ取得
-# pipInstall() {
-#     pip3 install -r $CWD/python3/
-# }
-
-
-# シンボリックリンク作成
 symLinks() {
-    # nvim
-    echo 'Create symlinks for vim'
-    ln -sf $CWD/nvim/init.vim ~/.vimrc
+    echo "Creating symbolic links..."
 
-    # nvim
-    echo 'Create symlinks for neovim'
-    ln -sf $CWD/nvim/init.vim ~/.config/nvim/init.vim
-    ln -sf $CWD/nvim/dein.toml ~/.config/dein/dein.toml
-    ln -sf $CWD/nvim/dein_lazy.toml ~/.config/dein/dein_lazy.toml
+    # Vim
+    ln -sf "$CWD/nvim/init.vim" ~/.vimrc
 
-    # fish
-    echo 'Create symlinks for fish'
-    ln -sf $CWD/fish/config.fish ~/.config/fish/config.fish
-    ln -sf $CWD/fish/aliases.fish ~/.config/fish/aliases.fish
-    ln -sf $CWD/fish/anyenv.fish ~/.config/fish/anyenv.fish
-    find $CWD/fish/conf.d/* -type f -print  | awk -F"/" '{ print $NF }' | xargs -I{} ln -sf $CWD/fish/conf.d/{} ~/.config/fish/conf.d/{}
-    find $CWD/fish/functions/* -type f -print  | awk -F"/" '{ print $NF }' | xargs -I{} ln -sf $CWD/fish/functions/{} ~/.config/fish/functions/{}
+    # Neovim
+    ln -sf "$CWD/nvim/init.vim" ~/.config/nvim/init.vim
+    ln -sf "$CWD/nvim/dein.toml" ~/.config/dein/dein.toml
+    ln -sf "$CWD/nvim/dein_lazy.toml" ~/.config/dein/dein_lazy.toml
 
-    # tmux
-    echo 'Create symlinks for tmux'
-    ln -sf $CWD/tmux/tmux.conf ~/.tmux.conf
-    ln -sf $CWD/tmux/tmuxline.conf ~/.tmux/tmuxline.conf
+    # Fish
+    ln -sf "$CWD/fish/config.fish" ~/.config/fish/config.fish
+    ln -sf "$CWD/fish/aliases.fish" ~/.config/fish/aliases.fish
+    find "$CWD/fish/conf.d" -type f -exec ln -sf {} ~/.config/fish/conf.d/{} \;
+    find "$CWD/fish/functions" -type f -exec ln -sf {} ~/.config/fish/functions/{} \;
 
-    # peco
-    echo 'Create symlinks for peco'
-    ln -sf $CWD/peco/config.json ~/.config/peco/config.json
+    # Tmux
+    ln -sf "$CWD/tmux/tmux.conf" ~/.tmux.conf
+    ln -sf "$CWD/tmux/tmuxline.conf" ~/.tmux/tmuxline.conf
 
-    # grc
-    echo 'Create symlinks for grc'
-    ln -sf $CWD/grc/conf.mtr ~/.grc/conf.mtr
+    # Peco
+    ln -sf "$CWD/peco/config.json" ~/.config/peco/config.json
 
-    # latexmk
-    echo 'Create symlinks for latexmk'
-    ln -sf $CWD/latexmk/latexmkrc ~/.latexmkrc
+    # GRC
+    ln -sf "$CWD/grc/conf.mtr" ~/.grc/conf.mtr
 
-    # karabiner
-    echo 'Create symlinks for karabiner'
-    ln -sf $CWD/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+    # Latexmk
+    ln -sf "$CWD/latexmk/latexmkrc" ~/.latexmkrc
+
+    # Karabiner
+    ln -sf "$CWD/karabiner/karabiner.json" ~/.config/karabiner/karabiner.json
+
+    echo "Symbolic links created successfully."
 }
 
-
-# 実行
-# pipInstall
 symLinks
-# anyenv-update
-git clone https://github.com/znz/anyenv-update.git "$(anyenv root)/plugins/anyenv-update"
